4e0afd2926c9421ad71e8f0b85d93275e06af01c
knakna <kna@tut.by>
2023-12-05 23:20:11 +0300

    fix  program to output text to the console

--- lesson-01/05.js
+++ lesson-01/05.js
@@ -19,9 +19,10 @@ switch (true) {
     case rule.length < 25:
       console.log("Все таки нет правил без исключения");
       break;
-    case rule.length === 25:
-      console.log("50 на 50");
+    case rule.length > 25:
+        console.log("У меня 100 пудов все получится");
+
       break;
     default:
-      console.log("У меня 100 пудов все получится");
+      console.log("50 на 50");
   }
